3b3514ea697f90c476eb0f08731ade3e281a6bda
Owen Gahagan <114256905+PostVizsla@users.noreply.github.com>
2024-05-22 16:32:03 -0400

    Working on event to be invoked when player is detected

--- PeaceJam/Assets/Scripts/LookForPlayer.cs
+++ PeaceJam/Assets/Scripts/LookForPlayer.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Experimental.GlobalIllumination;
 
 public class LookForPlayer : MonoBehaviour
@@ -17,10 +19,14 @@ public class LookForPlayer : MonoBehaviour
 
     // Variables for determining when the detector can see the player
     bool lookingUp;
-    double lookTimer = 0;
+    bool lookEnabled;
+    double lookTimer;
     public double lookThreshold;
 
     bool playerSeen; // test
+    UnityEvent Detected;
+    delegate void detectionDelegate(Transform source);
+    detectionDelegate detectedDialogue;
 
     // Start is called before the first frame update
     void Start()
@@ -33,18 +39,27 @@ public class LookForPlayer : MonoBehaviour
 
         // Starting state for whether the detector can see
         lookingUp = false;
+        lookEnabled = true;
         GetComponent<Renderer>().material.color = Color.green;
         viewLight.enabled = false;
         lookTimer = 0;
 
         playerSeen = false;
+
+        if(Detected == null)
+        {
+            Detected = new UnityEvent();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         // Increment the look timer
-        lookTimer += Time.deltaTime;
+        if (lookEnabled)
+        {
+            lookTimer += Time.deltaTime;
+        }
 
         // If the look timer exceeds the look threshold,
         // change whether the detector is looking, and reset the look timer
@@ -68,14 +83,22 @@ public class LookForPlayer : MonoBehaviour
         }
 
         // Check if the player is seen by the detector
-        if (GeometryUtility.TestPlanesAABB(planes, playerCollider.bounds) && lookingUp)
+        if (GeometryUtility.TestPlanesAABB(planes, playerCollider.bounds) && lookingUp && lookEnabled)
         {
             playerSeen = true;
             Debug.Log("Player seen by " + name);
+
+            //lookEnabled = false; // Stop checking for the player after they are initially detected
+            Detected.Invoke();
         }
         else
         {
             playerSeen = false;
         }
+    }
+
+    public void reEnableLooking()
+    {
+        lookEnabled = true;
     }
 }
